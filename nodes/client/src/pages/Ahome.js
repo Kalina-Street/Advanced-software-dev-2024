18eb746d76a8968e8721ec86d5ab34631c2b3c37
KazzyWazzyy <kkozibura84@gmail.com>
2024-05-05 21:56:47 +0100

    Update Ahome.js

--- nodes/client/src/pages/Ahome.js
+++ nodes/client/src/pages/Ahome.js
@@ -1,8 +1,21 @@
-import Logout from "../js/Logout"
+import { useEffect } from "react";
+import Logout from "../js/Logout";
+import Tabs from "../js/Tabs";
+import Aprofile from "./Aprofile";
+import Newstaff from "./Newstaff";
+import Staffsearch from "./Staffsearch";
+import Atask from "./Atask";
+import axios from "axios";
 export default function Ahome() {
-  
     return (
-    <Logout></Logout>
-
-    )
-};
+        <div id="outer">
+          <Tabs></Tabs>
+          <div id="rootx">
+            {/*<Atask></Atask>
+            <Staffsearch></Staffsearch>
+    <Newstaff></Newstaff>*/}
+          </div>
+          <Logout></Logout>
+        </div>
+      );
+    }
